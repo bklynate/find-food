da78de3b3b975cbc6e56b20857ba28beb7328528
bklynate <nathanielcfa@gmail.com>
2019-12-03 23:56:12 -0500

    feat: add functioning navigation; add search screen

---
+++ src/screens/SearchFoodScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchFoodScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchFoodScreen;

10485971f93e08c9ca30f38676e5325247aab128
bklynate <nathanielcfa@gmail.com>
2019-12-05 08:31:06 -0500

    feat: wired up searchbar component and searchbar screen

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = props => {
+  const { searchTerm, onSearchTermChange, onSearchTermSubmit } = props;
+
   return (
-    <View>
-      <TextInput placeholder='Search' />
+    <View style={styles.searchBarContainer}>
+      <Feather name='search' style={styles.featherIcon} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={searchTerm}
+        style={styles.searchInput}
+        onChangeText={onSearchTermChange}
+        placeholder='Search'
+        onEndEditing={onSearchTermSubmit}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  searchBarContainer: {
+    backgroundColor: '#f0eeee',
+    height: 45,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    paddingHorizontal: 10,
+    marginTop: 15,
+    flexDirection: 'row',
+  },
+
+  featherIcon: {
+    marginHorizontal: 15,
+    fontSize: 35,
+    alignSelf: 'center',
+  },
+
+  searchInput: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
 
 export default SearchBar;
--- src/screens/SearchFoodScreen.js
+++ src/screens/SearchFoodScreen.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from 'app/src/components/SearchBar.js';
 
 const SearchFoodScreen = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        searchTerm={searchTerm}
+        onSearchTermChange={setSearchTerm}
+        onSearchTermSubmit={() => console.log('Submitted')}
+      />
       <Text>Home Screen</Text>
     </View>
   );

efa8469a2cb8a6115194fe8ef3593780660bb4ea
bklynate <nathanielcfa@gmail.com>
2019-12-05 19:31:35 -0500

    feat: add ability to query yelp API; hide yelp api key

--- src/screens/SearchFoodScreen.js
+++ src/screens/SearchFoodScreen.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from 'app/src/components/SearchBar.js';
+import yelpAPI from 'app/src/api/yelpAPI';
 
 const SearchFoodScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const searchAPI = async () => {
+    const {
+      data: { businesses = [] },
+    } = await yelpAPI.get('/search', {
+      params: {
+        limit: 50,
+        term: searchTerm,
+        location: 'new york',
+      },
+    });
+    setSearchResults(businesses);
+  };
 
   return (
     <View>
       <SearchBar
         searchTerm={searchTerm}
         onSearchTermChange={setSearchTerm}
-        onSearchTermSubmit={() => console.log('Submitted')}
+        onSearchTermSubmit={searchAPI}
       />
-      <Text>Home Screen</Text>
+      <Text>We have found {searchResults.length} search results.</Text>
     </View>
   );
 };
